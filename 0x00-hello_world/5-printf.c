d93cde7266c08fae586baabb1023a13db44ec93c
Fraolasefa <132439223+Fraolasefa@users.noreply.github.com>
2023-06-17 17:30:50 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,13 @@
-/*
- * File: 5-printf.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is
- *                a piece of art," followed by a new line.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
+
